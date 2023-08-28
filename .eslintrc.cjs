b7fe6bf2a94a31b6ce65e97be9cf08719089cff9
Mango Sánchez-Redondo <eduardo.sanchezredondo@gmail.com>
2023-08-28 16:52:53 +0200

    feat: create app with vite

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "@typescript-eslint/explicit-function-return-type": "off"
+    }
+}
