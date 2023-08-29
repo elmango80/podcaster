7f619d3cb3b390345ce7cbf8eeb5d4f9db8377a1
Mango Sánchez-Redondo <eduardo.sanchezredondo@gmail.com>
2023-08-29 18:32:56 +0200

    feat:  add error handling in the fetcher

--- src/service/fetcher.ts
+++ src/service/fetcher.ts
@@ -1,17 +1,24 @@
 export const getFetch = async ({ url }: { url: string }) => {
-  const response = await fetch(
-    `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+  try {
+    const response = await fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch top podcasts.')
+    if (!response.ok) {
+      throw new Error('Failed to fetch top podcasts.')
+    }
+
+    const json = await response.json()
+
+    return JSON.parse(json?.contents) ?? {}
+  } catch (error) {
+    console.error(error)
+
+    return {}
   }
-
-  const json = await response.json()
-  return JSON.parse(json?.contents) ?? {}
 }
