faf40d67c65896992d567fd77cca8060ea26c300
Mango Sánchez-Redondo <eduardo.sanchezredondo@gmail.com>
2023-09-01 18:19:12 +0200

    feat: configure the application for development and production

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,29 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+
+  return {
+    plugins: [react(), createHtmlPlugin({ minify: isProduction })],
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    watch: {
-      ignored: ['**/coverage/**'],
+    server: {
+      watch: {
+        ignored: ['**/coverage/**'],
+      },
     },
-  },
+    build: {
+      cssMinify: isProduction,
+      minify: isProduction,
+      sourcemap: isProduction ? 'inline' : true,
+      emptyOutDir: true,
+      rollupOptions: {},
+    },
+  }
 })
